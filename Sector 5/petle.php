be35c3538fd1f555c4f962f7d8dc1659677832be
PiosX <piosso.pios@op.pl>
2021-06-02 20:59:07 +0200

    Petle

---
+++ Sector 5/petle.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pętle</title>
+</head>
+<body>
+    <?php
+/*
+        for($i = 0;$i<3;$i++)
+        {
+            echo $i."<br />";
+        }
+
+        $j = 3;
+
+        while($j--)
+        {
+            echo $j."<br />";
+
+        }
+
+        echo "\$i = ".$i;
+        echo "\$j = ".$j;
+*/
+        $k = 5;
+        do{
+            echo $k."<br />";
+            //$k++;
+        }while($k != 5)
+
+    ?>
+</body>
+</html>
