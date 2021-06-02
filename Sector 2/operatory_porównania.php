65c0f77d7a4d80b228c4bccf768e2fed2a6fd3ce
PiosX <piosso.pios@op.pl>
2021-06-02 08:51:25 +0200

    Operatory

---
+++ Sector 2/operatory_porównania.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Operatory Porównania</title>
+</head>
+<body>
+    <?php
+        $zm = 5;
+        $zm2 = '5';
+
+        //BOOL = true - 1   false - 0(nic)
+
+      // var_dump($zm == $zm2); //dwa znaki równości służą do porównania
+      // var_dump($zm != $zm2);
+      //var_dump($zm <> $zm2); //czy są różne
+      //var_dump($zm > $zm2);
+      //var_dump($zm < $zm2);
+      //var_dump($zm <= $zm2);
+
+
+      //var_dump($zm === $zm2);
+      var_dump($zm !== $zm2);
+
+    ?>
+</body>
+</html>
