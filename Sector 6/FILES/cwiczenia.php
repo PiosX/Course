1b9e2d6e5050d032e7aedb92f6461c2f902e188a
PiosX <piosso.pios@op.pl>
2021-06-05 20:09:25 +0200

    Zmienne predefiniowane

---
+++ Sector 6/FILES/cwiczenia.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="cwiczenia.php" method="POST" enctype="multipart/form-data">
+        <div>
+            <input type="hidden" name="MAX_FILE_SIZE" value="30720" />
+            <input type="file" name="plik" />
+            <input type="submit" value="Dodaj plik" />
+        </div>
+    </form>
+    <?php
+        if(isset($_FILES['plik']))
+        {
+
+            switch($_FILES['plik']['error'])
+            {
+                case 0:
+                    if($_FILES['plik']['type'] == "image/jpeg" || $_FILES['plik']['type'] == "image/png")
+                    {
+                        move_uploaded_file($_FILES['plik']['tmp_name'], "images/".md5(rand()*rand()+rand()).$_FILES['plik']['name']);
+                        echo "Plik pomyślnie wysłany!";
+                    }else{
+                        echo "Niedozwolone rozszerzenie pliku.";
+                    }
+                    break;
+                case 1:
+                    echo "Error";
+                    break;
+                case 2:
+                    echo "Error";
+                    break;
+                case 3:
+                    echo "Error";
+                    break;
+                case 4:
+                    echo "Error";
+                    break;
+                default:
+                    echo "Error#";
+                    break;
+            }
+
+        }
+    ?>
+</body>
+</html>
