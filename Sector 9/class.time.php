e9398e8adcf0d291dca31c8c2992ebf536a6902d
PiosX <piosso.pios@op.pl>
2021-06-07 10:08:48 +0200

    Obiektowe PHP

---
+++ Sector 9/class.time.php
@@ -0,0 +1,39 @@
+<?php
+
+class Time
+{
+    const DEFAULT_TIME_ZONE = "Europe/Warsaw";
+    /**
+     * @var $currentTime = przechowuje aktualny czas względem DEFAULT_TIME_ZONE 
+     */
+    public $currentTime;
+    public $futureTime;
+    public $timeZone;
+    function __construct($timeZone=self::DEFAULT_TIME_ZONE)//odwoływanie się do siebie self:: zeby moc sie odwolac do stalej
+    {
+        $this->timeZone = $timeZone;
+        date_default_timezone_set($this->timeZone);
+        $this->currentTime = $this->getCurrentTime();
+    }
+    function __toString()
+    {
+        return $this->currentTime;
+    }
+    function setFutureTime($days, $pattern="H:i:s d M Y")
+    {
+        $this->setTimeZone();
+        $this->futureTime = date($pattern, strtotime("+ $days days"));
+        return $this->futureTime;
+    }
+    function getCurrentTime($pattern="H:i:s d M Y")
+    {
+        $this->setTimeZone();
+        return date($pattern, time());
+    }
+    function setTimeZone()
+    {
+        if($this->timeZone != date_default_timezone_get())
+            date_default_timezone_set($this->timeZone);
+    }
+}
+?>
---
+++ Sector 9/klasyobiekty.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Klasy - obiekty</title>
+</head>
+<body>
+    <?php
+        require_once("class.time.php");
+
+        $a = new Time();
+        $b = new Time("America/Atka");
+        $c = new Time("Australia/Yancowinna");
+
+        echo $a->setFutureTime(5)."<br />";
+        echo $b->setFutureTime(10)."<br />";
+
+        echo $a->futureTime."<br />";
+        $b->setFutureTime(2);
+        echo $b->futureTime."<br />";
+
+        echo $a."<br />";
+        echo $b."<br />";
+        echo $c."<br />";
+
+        $a->setFutureTime("4");
+        echo $a->futureTime."<br />";
+        echo $a->timeZone."<br />";
+        echo $b->timeZone."<br />";
+    ?>
+</body>
+</html>
