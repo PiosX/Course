37ef4c03629956fb31dd9b8bfd5ffb9220102b08
PiosX <piosso.pios@op.pl>
2021-06-09 18:09:05 +0200

    MySQL

---
+++ Sector 10/MySQL_PODSTAWY/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Podstawy MySQL</title>
+</head>
+<body>
+    <?php
+        //mysqli_connect("nazwa_serwera:port", "login", "haslo")
+        $mysqliConnection = @mysqli_connect("localhost", "root", "") or die("Nie udało się nawiązać połączenia z bazą danych.");
+
+        mysqli_select_db($mysqliConnection, "kursmysql") or die("Brak połączenia z bazą danych.");
+
+        echo "test";
+
+        mysqli_close($mysqliConnection);
+    ?>
+</body>
+</html>
