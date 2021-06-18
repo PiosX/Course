e7aef2ae8b358a2d177063269860cf0d50584b40
PiosX <piosso.pios@op.pl>
2021-06-18 11:46:10 +0200

    MySQLi

---
+++ Sector 11/TRIGGERY/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Triggery</title>
+</head>
+<body>
+    <?php
+    require_once("MultiQueryArray.php");
+        $conn = @mysqli_connect("localhost", "root", "", "kursmysql") or die(mysqli_connect_error());
+
+        mysqli_set_charset($conn, "utf8");
+        /*
+            CREATE TRIGGER trigger_name trigger_time trigger_event
+            ON tbl_name FOR EACH ROW trigger_stmt
+
+            trigger time: AFTER, BEFORE
+            trigger_event: INSERT, DELETE, UPDATE
+        */
+        $query = "
+            
+        ";
+        
+
+        try
+        {
+            $mq = new MultiQueryArray($conn);
+
+            $result = $mq->multi_query($query, MultiQueryArray::AS_DUMP);
+            
+        }catch(Exception $e)
+        {
+            echo $e->getMessage();
+        }      
+
+        mysqli_close($conn);
+    ?>
+</body>
+</html>
